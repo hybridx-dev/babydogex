4c2bb82bf6eb1225fb84dcbdc6693232feef779d
ramlan442 <ramlanajaa@gmail.com>
2024-05-15 09:26:35 +0700

    feat: handleMintNFT

--- src/components/Tools/MintNFT/MintNFT.tsx
+++ src/components/Tools/MintNFT/MintNFT.tsx
@@ -1,7 +1,13 @@
+import { formatUnits } from "viem";
 import { BalanceBabyDogeX } from "../../Balance";
 import { Button } from "../../ui/button";
+import { useMintNFT } from "./hooks";
+import { calculationFee } from "../../../lib";
+import { BiLoader } from "react-icons/bi";
 
 export const MintNFT = () => {
+    const { amount, fee, handleMintNFT, loadingMint, setAmount } = useMintNFT();
+
     return ( 
         <div className="p-4 space-y-2">
             <div className="flex gap-2 text-xs">
@@ -11,19 +17,21 @@ export const MintNFT = () => {
                     <p>$BABYDOGEX</p>
                 </div>
             </div>
-            <form className="flex items-center">
+            <form className="flex items-center" onSubmit={handleMintNFT}>
                 <input 
                     placeholder="Amount to Mint" 
                     name="amount" 
                     type="number" 
+                    onChange={(e) => setAmount(+e.target.value)}
+                    value={amount || ''}
                     className="flex-grow p-2 rounded-l-md"
                 />
-                <Button type="submit" className={`bg-yellow-500 hover:bg-yellow-600 text-white flex-1 block font-bold p-2 rounded-r-md rounded-l-none`}>
-                    MINT
+                <Button type="submit" disabled={!amount || loadingMint} className={`bg-yellow-500 hover:bg-yellow-600 text-white flex-1 block font-bold p-2 rounded-r-md rounded-l-none`}>
+                    {loadingMint ? <BiLoader className="animate-spin mx-auto" size={24}/> : 'MINT'}
                 </Button>
             </form>
 
-            <span className="text-xs text-center w-full block font-medium text-white">Fee: 1000</span>
+            <span className="text-xs text-center w-full block font-medium text-white">Fee: {fee && amount ? formatUnits(calculationFee(fee, amount), 18) : ''}</span>
         </div>
     );
 }
---
+++ src/components/Tools/MintNFT/hooks/useMintNFT.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { checkFee, minNFT } from "../../../../api";
+import { toast } from "react-toastify";
+import { type RpcError } from "viem";
+import { waitForTransactionReceipt } from "wagmi/actions";
+import { config } from "../../../../config";
+
+export const useMintNFT = () => {
+    const [amount, setAmount] = useState<undefined | number>();
+    const [fee, setFee] = useState(BigInt(0));
+    const [loadingMint, setLoadingMint] = useState(false);
+
+    useEffect(() => {
+        checkFee().then(setFee);
+    }, []);
+
+    const handleMintNFT = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoadingMint(true);
+
+        try {
+            if (amount && fee) {
+                alert(amount)
+                const tx = await minNFT(fee, amount);
+                await waitForTransactionReceipt(config, {
+                    hash: tx,
+                    retryDelay: 1500,
+                });
+                // TODO: update balance;
+                setAmount(0);
+                toast.success('success');
+            }            
+        } catch (error) {
+            toast.error((error as RpcError).shortMessage);
+        }
+        finally{
+            setLoadingMint(false)
+        }
+    }
+
+    return {
+        amount,
+        setAmount,
+        fee,
+        loadingMint,
+        handleMintNFT
+    }
+}
